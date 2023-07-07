666474c510c57921d336c68e21cea245c1a6f401
Gabriel Cánepa <gacanepa@gmailcom>
2023-07-07 14:45:00 -0300

    Add comment to match versions in requirements.txt

--- section4/spamdetection/test-packages.py
+++ section4/spamdetection/test-packages.py
@@ -5,7 +5,8 @@ import pandas
 import sklearn
 import textblob
 
-print(f"numpy version: {numpy.__version__}")  # Should be 1.25.0 or higher
-print(f"pandas version: {pandas.__version__}")  # 2.0.3 or higher
-print(f"scikit-learn version: {sklearn.__version__}")  # 1.2.2 or higher
-print(f"textblob_version: {textblob.__version__}")  # 0.17.1 or higher
+# The versions listed above are pinned in the requirements file
+print(f"numpy version: {numpy.__version__}")  # Should be 1.25.0
+print(f"pandas version: {pandas.__version__}")  # 2.0.3
+print(f"scikit-learn version: {sklearn.__version__}")  # 1.2.2
+print(f"textblob_version: {textblob.__version__}")  # 0.17.1
